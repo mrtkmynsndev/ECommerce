2824b7dee13728f8fada5fa9ebeec2fd4a789e04
Mert Kimyonşen <mert.kimyonsen@netone.net.tr>
2020-07-24 23:18:47 +0300

    Angular Basics

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPagination } from "./features/models/pagination";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dashboard Pannel';
+  products: any[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/product?pagesize=25').subscribe((response: IPagination) => {
+      this.products = response.data;
+    },
+      err => {
+        console.log(err);
+      });
+  }
 }
+
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from './features/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
